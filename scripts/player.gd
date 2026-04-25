f92289be5ba7ce856f9988fcfaf917414aa12052
KarlPham <phamkhoaa819@gmail.com>
2026-04-25 22:36:26 +1000

    feat: add jump and run animation

--- scripts/player.gd
+++ scripts/player.gd
@@ -6,6 +6,7 @@ const JUMP_VELOCITY = -300.0
 @onready var animated_sprite = $AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
+
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
@@ -16,6 +17,15 @@ func _physics_process(delta: float) -> void:
 
 	# Get the input direction and handle the movement/deceleration.
 	var direction := Input.get_axis("move_left", "move_right")
+	
+	#Animation
+	if is_on_floor():
+		if direction == 0:
+			animated_sprite.play("idle")
+		else:
+			animated_sprite.play("run")
+	else:
+		animated_sprite.play("jump")
 	
 	#Flip the sprite
 	if direction > 0:
